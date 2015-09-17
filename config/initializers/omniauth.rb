c69540d87203ecf232d3ce3bafcd799c41f6c519
Phill Conrad <pconrad@cs.ucsb.edu>
2015-09-17 14:50:09 -0700

    get the FACEBOOK_APP_ID and FACEBOOK_SECRET from ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '', '', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  puts "Yo, here is dat thing: ENV[\"FACEBOOK_APP_ID\"]=" + ENV["FACEBOOK_APP_ID"] + "ENV[\"FACEBOOK_SECRET\"]=" + ENV["FACEBOOK_SECRET"]
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
